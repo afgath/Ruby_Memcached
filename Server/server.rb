247d6a9c0328021bd807bf784a4dd96c953d1a65
unknown <anfegiar@gmail.com>
2020-09-05 02:37:06 -0500

    Adding the LoadTest for the server ant reordering the solution in separated folders

--- Server/server.rb
+++ Server/server.rb
@@ -3,12 +3,12 @@ require 'time'
 require './item'
 
 
-cache = {}
-timer = nil
-hostname = 'localhost'
-port = 1997
-server = TCPServer.open(hostname, port)
-highest_id = 1
+cache = {} # Hash to save the items
+timer = nil # Thread that contains the flush_all if is timed
+hostname = '127.0.0.1'
+port = 11211
+server = TCPServer.new(hostname, port)
+highest_id = 1 # Id to be asigned on every item adding or modification
 
 loop do
   Thread.start(server.accept) do |socket| # Multithread started so it can serve multiple clients
@@ -18,7 +18,7 @@ loop do
     loop do
       if request.nil? # Case: is not first command it receives the new interactions
         request = socket.gets
-        request = request.chomp unless request.nil?
+        request = request.chomp unless request.nil? # To avoid chomp a nil
       end
       if request.nil? || request == ''
         request = 'empty'
@@ -26,19 +26,21 @@ loop do
       if request.upcase == "QUIT"
         break
       end
-      puts request[4, request.length]
+      puts request # To view received commands in console
       valid = true # Value that indicates If item is valid
       array_validate = request.split(" ")
+      # Validate all numeric values are positive
       if array_validate[2].to_i.negative? || array_validate[3].to_i.negative? || array_validate[4].to_i.negative?
         valid = false
       end
       item_validate = cache[array_validate[1]]
-      if !item_validate.nil? && item_validate.exptime.to_i.positive? && (Time.new-item_validate.time).to_i >= item_validate.exptime.to_i
+      # If item is expired already, then is deleted before any process
+      if !item_validate.nil? && item_validate.exptime.to_i.positive? && (Time.new - item_validate.time).to_i >= item_validate.exptime.to_i
         cache.delete(array_validate[1])
       end
       exist_value = false
       exist_value = true unless cache[array_validate[1]].nil?
-      if array_validate[0] == 'DELETE'
+      if array_validate[0] == 'DELETE' # Case Delete an item by key
         if array_validate.length < 2
           socket.write("ERROR\r\n")
         elsif array_validate.length > 2
@@ -49,20 +51,20 @@ loop do
           cache.delete(array_validate[1])
           socket.write("DELETED\r\n")
         end
-      elsif array_validate[0].upcase == 'FLUSH_ALL'
+      elsif array_validate[0].upcase == 'FLUSH_ALL' # Case flush all items in cache
         if array_validate.length > 2
           socket.write("ERROR\r\n")
         else
           if array_validate[1].nil? || array_validate[1].to_i.nil?
             cache = {}
             highest_id = 1
-          else
-            Thread.kill(timer) unless timer.nil?
+          else # Case flush_all command has a timer set
+            Thread.kill(timer) unless timer.nil? #Kills previous thread to replace the timer
             timer = Thread.new { sleep array_validate[1].to_i; ; cache = {}; highest_id = 1 }
           end
             socket.write("OK\r\n")
         end
-      elsif array_validate[0].upcase == 'INCR' || array_validate[0].upcase == 'DECR'
+      elsif array_validate[0].upcase == 'INCR' || array_validate[0].upcase == 'DECR' # Case numeric value to have a sum or substract
         if array_validate.length != 3 || cache[array_validate[1]].nil?
           socket.write("ERROR\r\n")
         elsif !cache[array_validate[1]].value.to_i.nil? && cache[array_validate[1]].value.to_i.positive? && !array_validate[2].to_i.nil? && array_validate[2].to_i.positive?
@@ -75,7 +77,7 @@ loop do
         elsif array_validate[2].to_i.nil? || !array_validate[2].to_i.positive?
           socket.write("CLIENT_ERROR invalid numeric delta argument\r\n")
         end
-      elsif array_validate[0].upcase == 'APPEND' || array_validate[0].upcase == 'PREPEND'
+      elsif array_validate[0].upcase == 'APPEND' || array_validate[0].upcase == 'PREPEND' # Case append a value after or before de current value
         if array_validate.length >= 5 && array_validate.length <= 6
           input = socket.gets.chomp
           if exist_value && input.to_s.length == array_validate[4].to_i && valid
@@ -95,7 +97,7 @@ loop do
           socket.write("ERROR\r\n")
         end
 
-      elsif array_validate[0].upcase == 'CAS'
+      elsif array_validate[0].upcase == 'CAS' # Case edit a value that haven't been edited before
         mark_exists = false
         mark_notfound = false
         mark_wronglength = false
@@ -129,11 +131,13 @@ loop do
           socket.write("ERROR\r\n")
         end
 
+        # Case all basic writing operations
       elsif array_validate[0].upcase == 'REPLACE' || array_validate[0].upcase == 'SET' || array_validate[0].upcase == 'ADD'
         apply = true # value that indicates If the operation applies
         if request[4, request.length].nil?
           valid = false
         end
+
         if request[0, 3].upcase == 'ADD' && cache.any? # If command=add then key must not exist
           apply = false unless cache[array_validate[1]].nil?
         end
@@ -166,10 +170,13 @@ loop do
             end
           end
         end
-      elsif array_validate[0].upcase == 'GETS' || array_validate[0].upcase == 'GET'
+      elsif array_validate[0].upcase == 'GETS' || array_validate[0].upcase == 'GET' # Case Fetch commands
         if array_validate.length == 2
+          # GETS
           value = 'VALUE ' + request[5, request.length] + ' ' + cache[request[5, request.length]].to_s + ' ' + cache[request[5, request.length]].id.to_s + "\r\n" + cache[request[5, request.length]].get_value unless cache[request[5, request.length]].nil? || array_validate[0].upcase == 'GET'
+          # GET
           value = 'VALUE ' + request[4, request.length] + ' ' + cache[request[4, request.length]].to_s + "\r\n" + cache[request[4, request.length]].get_value unless cache[request[4, request.length]].nil? || array_validate[0].upcase == 'GETS'
+
           socket.write(value) unless value.nil?
           socket.write("\r\n") unless value.nil?
           socket.write("END\r\n")
@@ -178,11 +185,6 @@ loop do
         end
       else
         socket.write("ERROR\r\n")
-      # elsif array_validate[0].upcase == 'GET'
-      #   value = 'VALUE ' + request[4, request.length] + ' ' + cache[request[4, request.length]].to_s + "\r\n" + cache[request[4, request.length]].get_value unless cache[request[4, request.length]].nil?
-      #   socket.write(value) unless value.nil?
-      #   socket.write("\r\n") unless value.nil?
-      #   socket.write("END\r\n")
       end
       request = nil
     end
